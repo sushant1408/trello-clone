83b30fc7f9cfe0c553a92c78506ac0f79a52e905
Sushant Gandhi <sushant@simplified.co>
2025-02-23 22:27:12 +0530

    displaying boards

--- src/app/(main)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ src/app/(main)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,9 +1,29 @@
+import { auth } from "@clerk/nextjs/server";
 import { HelpCircleIcon, User2Icon } from "lucide-react";
+import { redirect } from "next/navigation";
 
-import { TooltipWrapper } from "@/components/tooltip-wrapper";
 import { FormPopover } from "@/components/form/form-popover";
+import { TooltipWrapper } from "@/components/tooltip-wrapper";
+import { db } from "@/lib/db";
+import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
-const BoardList = () => {
+const BoardList = async () => {
+  const { orgId } = await auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const boards = await db.board.findMany({
+    where: {
+      orgId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="space-y-4">
       <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -11,6 +31,19 @@ const BoardList = () => {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        {boards.map((board) => (
+          <Link
+            key={board.id}
+            href={`/board/${board.id}`}
+            style={{
+              backgroundImage: `url(${board.imageThumbUrl})`,
+            }}
+            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+          >
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="relative font-semibold text-white">{board.title}</p>
+          </Link>
+        ))}
         <FormPopover sideOffset={10} side="right">
           <button className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
             <p className="text-sm">Create new board</p>
@@ -26,6 +59,18 @@ const BoardList = () => {
           </button>
         </FormPopover>
       </div>
+    </div>
+  );
+};
+
+BoardList.Skeleton = function BoardListSkeleton() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
     </div>
   );
 };
--- src/app/(main)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(main)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator";
 import { BoardList } from "./_components/board-list";
 import { Info } from "./_components/info";
+import { Suspense } from "react";
 
 export default async function OrganizationIdPage() {
   return (
@@ -8,7 +9,9 @@ export default async function OrganizationIdPage() {
       <Info />
       <Separator className="my-4" />
       <div className="px-2 md:px-4">
-        <BoardList />
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   );
